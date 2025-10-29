c34ed2eb77af8b9012aa4afc402f43b0d4f7a907
agentfl <239420562+agentfl@users.noreply.github.com>
2025-10-28 23:49:04 -0500

    Create ChatInterface.tsx

---
+++ app/app/components/ChatInterface.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ChatKit } from "@openai/chatkit/react";
+
+export default function ChatInterface() {
+  return (
+    <div
+      style={{
+        background: "white",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ChatKit
+        workflowId={process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID!}
+        style={{
+          width: "80vw",
+          height: "80vh",
+          borderRadius: "0",
+          backgroundColor: "white",
+          boxShadow: "none",
+        }}
+      />
+    </div>
+  );
+}
